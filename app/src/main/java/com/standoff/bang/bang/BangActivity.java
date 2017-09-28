a7dde5ab9bb67bd3395f3e4d928935950afd386a
Kelvin Ma <kelvinhanma@google.com>
2017-09-28 09:52:04 -0700

    start repo

---
+++ app/src/main/java/com/standoff/bang/bang/BangActivity.java
@@ -0,0 +1,13 @@
+package com.standoff.bang.bang;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class BangActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bang);
+    }
+}
