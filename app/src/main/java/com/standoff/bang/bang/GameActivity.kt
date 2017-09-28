b5a8d09c87bb98870447d2cbfeb81bb6d6ff5c5b
Ivan Gavrilovic <gavra@google.com>
2017-09-28 14:31:38 -0700

    Timer for the move

--- app/src/main/java/com/standoff/bang/bang/GameActivity.kt
+++ app/src/main/java/com/standoff/bang/bang/GameActivity.kt
@@ -8,19 +8,22 @@ import android.view.MotionEvent
 import android.view.View
 import android.view.animation.Animation
 import android.view.animation.AnimationUtils
+import android.widget.ProgressBar
 import android.widget.TextView
 import com.standoff.bang.bang.moves.MovementTracker
 import thirdparty.bindView
+import android.os.CountDownTimer
+import com.standoff.bang.bang.moves.GameAction
 
-/**
- * Created by gavra on 9/28/17.
- */
 
 class GameActivity : Activity(){
 
     var fadeOut: Animation? = null
 
     val actionTextView: TextView? by bindView(R.id.action_text_view)
+    val progressBar: ProgressBar? by bindView(R.id.timer)
+    var action: GameAction? = null
+    var timer: TimerWithProgressBar? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -34,6 +37,7 @@ class GameActivity : Activity(){
 
             override fun onAnimationEnd(p0: Animation?) {
                 actionTextView!!.visibility = View.INVISIBLE
+                startTimer()
             }
 
             override fun onAnimationStart(p0: Animation?) {
@@ -42,6 +46,8 @@ class GameActivity : Activity(){
 
         MovementTracker.init()
         actionTextView!!.startAnimation(fadeOut)
+
+        timer = TimerWithProgressBar(progressBar!!, this)
     }
 
     override fun onTouchEvent(event: MotionEvent?): Boolean {
@@ -53,11 +59,40 @@ class GameActivity : Activity(){
         return super.onTouchEvent(event)
     }
 
-    fun startCountdown() {
-
+    fun startTimer() {
+        timer!!.start()
     }
 
-    fun endCountdown() {
+    fun resetTimer() {
+        timer!!.reset()
+    }
 
+    fun playMove() {
+        // send the action to server
+    }
+}
+
+class TimerWithProgressBar(val bar: ProgressBar, val activity: GameActivity) {
+    var i = 0
+    val countdown = object : CountDownTimer(5000, 1000) {
+
+        override fun onTick(millisUntilFinished: Long) {
+            i++
+            bar.progress = i * 100 / (5000 / 1000)
+        }
+
+        override fun onFinish() {
+            i++
+            bar.progress = 100
+            activity.playMove()
+        }
+    }
+
+    fun start() {
+        countdown.start()
+    }
+
+    fun reset() {
+        countdown.cancel()
     }
 }
