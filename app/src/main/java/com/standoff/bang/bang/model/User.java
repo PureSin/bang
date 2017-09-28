ede617bc24e33025262c77f88272337192d6af26
Kelvin Ma <kelvinhanma@google.com>
2017-09-28 10:07:34 -0700

    added network dependencies

---
+++ app/src/main/java/com/standoff/bang/bang/model/User.java
@@ -0,0 +1,8 @@
+package com.standoff.bang.bang.model;
+
+/**
+ * Created by kelvinhanma on 9/28/17.
+ */
+
+public class User {
+}
---
+++ app/src/main/java/com/standoff/bang/bang/network/Room.java
@@ -0,0 +1,8 @@
+package com.standoff.bang.bang.network;
+
+/**
+ * Created by kelvinhanma on 9/28/17.
+ */
+
+class Room {
+}
